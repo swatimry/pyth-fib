0a564b37e4318368468730fdb683188d970810e0
Swati <115008404+swatimry@users.noreply.github.com>
2024-12-21 20:47:33 +0530

    Create recursive fibonaci

---
+++ fibonaci.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+n = int(input("Enter a number to find the Fibonacci sequence: "))
+
+
+print(f"Fibonacci of {n} is {fibonacci(n)}")
